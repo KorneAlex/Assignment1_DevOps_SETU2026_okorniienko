a43bee0522beea8cfdd837b1c2e01065e15d9ee1
KorneAlex <k0.orne@gmail.com>
2026-02-24 16:07:37 +0000

    working on create_buckets: error handlers

--- create_bucket.py
+++ create_bucket.py
@@ -3,57 +3,99 @@ import boto3
 import subprocess
 import webbrowser
 import time
-from helpers import id_generator
+from helpers import id_generator, bcolors
 
 s3 = boto3.resource("s3")
+files_to_upload = [
+    {'name':'indsex.html','type':'text/html'},
+    {'name':'error.html','type':'text/html'}, 
+    {'name':'logo.jpg','type':'image/jpeg'}
+    ]
     
 def create_bucket():
     bucket_name = 'okorniienko-' + str(id_generator())
-    get_img = subprocess.run(["curl", "-L", "-o", "www/logo.jpg", "http://devops.setudemo.net/logo.jpg"])
-
     # TODO: check if exist and create another one if true
-    bucket = s3.create_bucket(Bucket=bucket_name)
-    s3_client = boto3.client('s3')
-    s3_client.delete_public_access_block(Bucket=bucket_name)
+    print(bcolors.HEADER + f"Creating a bucked with name {bcolors.BOLD}{bcolors.OKGREEN}{bucket_name}" + bcolors.ENDC)
+    print("├── Uploading index.html, error.html and aws.jpg to the bucket...")
     try:
-        website_configuration = {
-            'ErrorDocument': {'Key': 'error.html'},
-            'IndexDocument': {'Suffix': 'index.html'},
-        }
-        
-        bucket_policy='''{
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Sid": "PublicReadGetObject",
-                    "Effect": "Allow",
-                    "Principal": "*",
-                    "Action": [
-                        "s3:GetObject"
-                    ],
-                    "Resource": [
-                        "arn:aws:s3:::%s/*"
-                    ]
-                }
-            ]
-        }''' % bucket_name
-        s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
-        time.sleep(2)
-        bucket_website = s3.BucketWebsite(bucket_name)
-        time.sleep(2)
-        response = bucket_website.put(WebsiteConfiguration=website_configuration)
-        time.sleep(2)
-        print("Uploading index.html and aws.jpg to the bucket...")
-        upload_file = s3.Object(bucket_name, "index.html").put(Body=open("www/index.html", 'rb'), ContentType='text/html')
-        upload_file = s3.Object(bucket_name, "error.html").put(Body=open("www/error.html", 'rb'), ContentType='text/html')
-        upload_file = s3.Object(bucket_name, "logo.jpg").put(Body=open("www/logo.jpg", 'rb'), ContentType='image/jpeg')
-        website_url = "http://%s.s3-website-%s.amazonaws.com" % (bucket_name, boto3.Session().region_name)
-        print("Bucket created and configured for website hosting. Access it at: " + website_url)
-        webbrowser.open(website_url)
-        return website_url
+        bucket = s3.create_bucket(Bucket=bucket_name)
+        s3_client = boto3.client('s3')
+        s3_client.delete_public_access_block(Bucket=bucket_name)
+        try:
+            website_configuration = {
+                'ErrorDocument': {'Key': 'error.html'},
+                'IndexDocument': {'Suffix': 'index.html'},
+            }
+            
+            bucket_policy='''{
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "PublicReadGetObject",
+                        "Effect": "Allow",
+                        "Principal": "*",
+                        "Action": [
+                            "s3:GetObject"
+                        ],
+                        "Resource": [
+                            "arn:aws:s3:::%s/*"
+                        ]
+                    }
+                ]
+            }''' % bucket_name
+            s3_client.put_bucket_policy(Bucket=bucket_name, Policy=bucket_policy)
+            time.sleep(2)
+            bucket_website = s3.BucketWebsite(bucket_name)
+            time.sleep(2)
+            response = bucket_website.put(WebsiteConfiguration=website_configuration)
+            time.sleep(2)
+            get_img = subprocess.run(["curl", "-L", "-o", "www/logo.jpg", "http://devops.setudemo.net/logo.jpg"])
+            print("Uploading index.html, error.html and aws.jpg to the bucket...")
+            for file in files_to_upload:
+                try:
+                    result = s3.Object(bucket_name, f'{file['name']}').put(Body=open(f'www/{file['name']}', 'rb'), ContentType=f'{file['type']}')
+                    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+                        print(f"[ {bcolors.OKGREEN}OK{bcolors.ENDC} ] Uploading {file['name']}")
+                    else:
+                        print(f"[ {bcolors.FAIL}ERR{bcolors.ENDC} ] Uploading {file['name']}")
+                except Exception as error:
+                    print(f"[ {bcolors.FAIL}ERR{bcolors.ENDC} ] Uploading {file['name']} --> ", error)
+                    
+            website_url = "http://%s.s3-website-%s.amazonaws.com" % (bucket_name, boto3.Session().region_name)
+            print("Bucket created and configured for website hosting. Access it at: " + website_url)
+            webbrowser.open(website_url)
+            return website_url
 
+        except Exception as error:
+            print(error)
     except Exception as error:
-        print(error)
+        print(bcolors.FAIL +bcolors.BOLD + f"\nError creating the bucket\n{error}\n" + bcolors.ENDC)
         
 if __name__ == "__main__":
-    create_bucket()
+    # create_bucket()
+    bucket_name = "okorniienko-vzpvcz"
+    # files_to_upload = [{'name':'indsex.html','type':'text/html'}, {'name':'error.html','type':'text/html'}, {'name':'logo.jpg','type':'image/jpeg'}]
+    # print(files_to_upload)
+    # for file in files_to_upload:
+    #     try:
+    #         result = s3.Object(bucket_name, f'{file['name']}').put(Body=open(f'www/{file['name']}', 'rb'), ContentType=f'{file['type']}')
+    #         if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+    #             print(f"[ {bcolors.OKGREEN}OK{bcolors.ENDC} ] Uploading {file['name']}")
+    #         else:
+    #             print(f"[ {bcolors.FAIL}ERR{bcolors.ENDC} ] Uploading {file['name']}")
+    #     except Exception as error:
+    #         print(f"[ {bcolors.FAIL}ERR{bcolors.ENDC} ] Uploading {file['name']} --> ", error)
+    # files_to_download = [
+    #     {
+    #         "link":"http://devops.setudemo.net/logo.jpg",
+    #     }
+    # ]
+    
+    check_link = subprocess.run('curl --silent --head http://devops.setudemo.net/logo.jpg | grep "200 OK"', shell=True, capture_output=True, text=True)
+    print(check_link.stdout)
+    if check_link.stdout:
+        get_img = subprocess.run(["curl", "--silent", "-L", "-o", "www/logo.jpg", "http://devops.setudemo.net/logo.jpg"])
+        print(f"[ {bcolors.OKGREEN}OK{bcolors.ENDC} ] Checking link")
+    else:
+        print(f"[ {bcolors.FAIL}ERR{bcolors.ENDC} ] Checking link --> {bcolors.FAIL}Invalid link{bcolors.ENDC}") 
+    # print(wget_img)
--- helpers.py
+++ helpers.py
@@ -6,6 +6,23 @@
 
 import random
 import string
+import subprocess
 
 def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+# Source - https://stackoverflow.com/a/287944
+# Posted by joeld, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-24, License - CC BY-SA 4.0
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
